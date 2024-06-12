531340b85b236eba758926b5cb5707e3bd1b2b34
henry112q <griffithseuan01@icloud.com~>
2024-06-12 15:11:40 +0100

    challange complete

---
+++ interactive-pricing-component-main/index.js
@@ -0,0 +1,30 @@
+function updateValue(val) {
+    var pageviews = document.getElementById("pageviewsamount")
+    pageviews.innerHTML = val
+    var amountpermonth = Math.ceil(val*(4/25)*100)/100
+    updatemoney(amountpermonth)
+    var slider = document.getElementById("myrange")
+    slider.setAttribute("value", val)
+}
+function updatemoney(amountpermonth) {
+    var money = document.getElementById("money")
+    var monthoryear = document.getElementById("month")
+    if (monthoryear.innerHTML == "month") {
+        money.innerHTML = amountpermonth
+    }
+    else {
+        var amountperyear = Math.ceil((amountpermonth*12)*0.75*100)/100
+        money.innerHTML = amountperyear
+    }
+}
+function Checkbox(val) {
+    var monthoryear = document.getElementById("month")
+    var slider = document.getElementById("myrange")
+    if(monthoryear.innerHTML == "month") {
+        monthoryear.innerHTML = "year"
+    }
+    else {
+        monthoryear.innerHTML = "month"
+    }
+    updateValue(slider.getAttribute("value"))
+}
