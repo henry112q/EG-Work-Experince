eb44bb7560ff8f9eb6ef9c2f509b890241e9606c
henry112q <griffithseuan01@icloud.com~>
2024-06-13 11:48:50 +0100

    commit

---
+++ age-calculator-app-main/index.js
@@ -0,0 +1,62 @@
+function DayValidation(val) {
+    var Day = document.getElementById("Day")
+    var month = document.getElementById("Month")
+    var year =document.getElementById("Year")
+    if(isNaN(val)) {
+        console.log("day is NAN")
+        return
+    }
+    if (month.value == "" && val > 31) {
+        console.log("day to long month undifined")
+        return
+    }
+    if (val > innerif()) {
+        console.log("day too long")
+        return
+    }
+}
+function innerif() {
+    var month = document.getElementById("Month")
+    var year =document.getElementById("Year")
+    if (month.value == 2 && year.value%4 == 0) {
+        return 29
+    }
+    else if (month.value == 2) {
+        return 28
+    }
+    else if (month.value % 2 == 0){
+        return 30
+    }
+    else {
+        return 31
+    }
+}
+function MonthValidation(val) {
+    var Day = document.getElementById("Day")
+    var month = document.getElementById("Month")
+    var year =document.getElementById("Year")
+    if(isNaN(val)) {
+        console.log("month is NAN")
+        return
+    }
+    if (val > 12) {
+        console.log("month too long")
+    }
+    if (year.value == 2024 && val > new Date().getMonth() + 1 ) {
+        console.log("month too long 22", val, new Date().getMonth())
+    }   
+    DayValidation(Day.value)
+}
+function YearValidation(val) {
+    var Day = document.getElementById("Day")
+    var month = document.getElementById("Month")
+    var year =document.getElementById("Year")
+    if(isNaN(val)) {
+        console.log("year is NAN")
+        return
+    }
+    if (val > new Date().getFullYear()){
+        console.log("year too long")
+    }
+    MonthValidation(month.value)
+}
